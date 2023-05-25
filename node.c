b1d46f9de1689bfb4f98523e111804b79c95ed04
KshitijBhat <me200003042@iiti.ac.in>
2023-05-25 12:46:22 -0400

    try pub sub node

--- node.c
+++ node.c
@@ -9,19 +9,49 @@
 
 #include "pico/stdlib.h"
 #include "pico_uart_transports.h"
+#include "hardware/pwm.h"
+
 
 const uint LED_PIN = 25;
+const uint ENA = 15;
+const uint IN1 = 14;
+const uint IN2 = 13;
 const uint encoderA = 16;
 const uint encoderB = 17;
 
-
-int count = 0; 
+const uint SPEED_FACTOR = 1;
+int encoder_count = 0; 
 int aState;
 int aLastState;  
+int pwm_level = 50;
 
 
 rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 motor_msg;
+std_msgs__msg__Int32 encoder_msg;
+
+
+void command_motor(int speed)
+{
+    if(speed>0){
+        gpio_put(IN1, 0);  //spin forward
+        gpio_put(IN2, 1);
+    }
+    else if(speed<0){
+        gpio_put(IN1, 1);  //spin forward
+        gpio_put(IN2, 0);
+    }
+    else{
+        gpio_put(IN1, 0);  //stop
+        gpio_put(IN2, 0);
+    }
+    int pwm_level = (int)abs(speed)*SPEED_FACTOR;
+    uint slice_num = pwm_gpio_to_slice_num(ENA);
+    pwm_set_wrap(slice_num, 255);
+    pwm_set_chan_level(slice_num, PWM_CHAN_B, pwm_level);
+    pwm_set_enabled(slice_num, true);
+}
 
 void counter()
 {
@@ -30,10 +60,10 @@ void counter()
     if (aState != aLastState){     
         // If the encoderB state is different to the encoderA state, that means the encoder is rotating clockwise
         if (gpio_get(encoderB) != aState) { 
-        count ++;
+        encoder_count ++;
         } 
         else {
-        count --;
+        encoder_count --;
         }
         // Serial.print("Position: ");
         // Serial.println(counter);
@@ -43,9 +73,20 @@ void counter()
 
 void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
-    rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
+    rcl_ret_t ret = rcl_publish(&publisher, &encoder_msg, NULL);
     counter();
-    msg.data = count; ////////////////////////////////////////////// encoder count
+    encoder_msg.data = encoder_count; ////////////////////////////////////////////// encoder count
+}
+
+void motor_callback(const void * msgin)
+{
+    const std_msgs__msg__Int32 * motor_msg = (const std_msgs__msg__Int32 *)msgin;
+    pwm_level = motor_msg->data;
+    command_motor(pwm_level);
+    gpio_put(LED_PIN, 1);
+    sleep_ms(1000); 
+    gpio_put(LED_PIN, 0);
+    sleep_ms(1000);
 }
 
 int main()
@@ -59,25 +100,31 @@ int main()
 		pico_serial_transport_read
 	);
 
+    // Setup all the pins
+
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    gpio_init(IN1);
+    gpio_set_dir(IN1, GPIO_OUT);
+
+    gpio_init(IN2);
+    gpio_set_dir(IN2, GPIO_OUT);
+
+    gpio_set_function(ENA, GPIO_FUNC_PWM);
+    
     gpio_init(encoderA);
     gpio_set_dir(encoderA, GPIO_IN);
-    
+
     gpio_init(encoderB);
     gpio_set_dir(encoderB, GPIO_IN);
 
     aLastState = gpio_get(encoderA);
+   
 
-    rcl_timer_t timer;
-    rcl_node_t node;
-    rcl_allocator_t allocator;
+    rcl_allocator_t allocator = rcl_get_default_allocator();
     rclc_support_t support;
-    rclc_executor_t executor;
-
-    allocator = rcl_get_default_allocator();
-
+    
     // Wait for agent successful ping for 2 minutes.
     const int timeout_ms = 1000; 
     const uint8_t attempts = 120;
@@ -90,32 +137,48 @@ int main()
         return ret;
     }
 
+    // create init_options
     rclc_support_init(&support, 0, NULL, &allocator);
 
+    // create node 
+    rcl_node_t node;
     rclc_node_init_default(&node, "pico_node", "", &support);
+
+    // create publisher
     rclc_publisher_init_default(
         &publisher,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
         "joint_state");
 
+    // create subscriber
+    rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"motor_command");
+
+    // create timer
+    rcl_timer_t timer;
     rclc_timer_init_default(
         &timer,
         &support,
         RCL_MS_TO_NS(1),
         timer_callback);
 
-    rclc_executor_init(&executor, &support.context, 1, &allocator);
+    // create executor
+    rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+    rclc_executor_init(&executor, &support.context, 2, &allocator);
     rclc_executor_add_timer(&executor, &timer);
+    rclc_executor_add_subscription(&executor, &subscriber, &motor_msg, &motor_callback, ON_NEW_DATA);
 
-    gpio_put(LED_PIN, 1);
-    
+    encoder_msg.data = 0; //default
 
-    msg.data = 0;
-    while (true)
-    {
-        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-    }
+    rclc_executor_spin(&executor);
+
+    rcl_subscription_fini(&subscriber, &node);
+    rcl_publisher_fini(&publisher, &node);
+	rcl_node_fini(&node);
     return 0;
 }
 
