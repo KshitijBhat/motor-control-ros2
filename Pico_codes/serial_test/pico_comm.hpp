ad2da4966716c60aa6c1f47368ff34f3295fa1b8
KshitijBhat <me200003042@iiti.ac.in>
2023-05-30 12:46:54 -0400

    improve structure

--- Pico_codes/serial_test/pico_comm.hpp
+++ Pico_codes/serial_test/pico_comm.hpp
@@ -7,82 +7,92 @@
 #define PI 3.1416
 #define ENCODER_TICKS_PER_REV 660
 
+class PicoComms{
+    public:
+        void sendchr(std::string data_byte)
+        {
+            // Write the data to the serial port.
+            serial_port_.Write(data_byte) ;
 
-constexpr const char* const SERIAL_PORT_2 = "/dev/ttyACM0" ;
-using namespace LibSerial ;
-// Instantiate a SerialPort object.
-SerialPort serial_port ;
-size_t ms_timeout = 10 ;
+            // Wait until the data has actually been transmitted.
+            serial_port_.DrainWriteBuffer();
 
-void sendchr(std::string data_byte)
-{
-    // Write the data to the serial port.
-    serial_port.Write(data_byte) ;
+            // Print to the terminal what is being written to the serial port.
+            std::cout << data_byte;
+        }
 
-    // Wait until the data has actually been transmitted.
-    serial_port.DrainWriteBuffer();
+        void writeMotor(int number)
+        {
+            if(number>255){
+                number = 255;
+            }
+            else if(number<-255){
+                number = -255;
+            }
+            std::string string_data = std::to_string(number) + ".";
+            sendchr(string_data);
+        }
 
-    // Print to the terminal what is being written to the serial port.
-    std::cout << data_byte;
-}
+        float readEncoder()
+        {
+            std::string read_buffer ;
+            sendchr("\r");
+            int encoder_val;
+            try
+            {
+                // Read as many bytes as are available during the timeout period.
+                serial_port_.Read(read_buffer, 0, ms_timeout) ;
+            }
+            catch (const LibSerial::ReadTimeout&)
+            {
+                encoder_val = stoi(read_buffer);
+            }
+            return encoder_val*2*PI/ENCODER_TICKS_PER_REV;
+        }
 
-void writeMotor(int number)
-{
-    if(number>255){
-        number = 255;
-    }
-    else if(number<-255){
-        number = -255;
-    }
-    std::string string_data = std::to_string(number) + ".";
-    sendchr(string_data);
-}
+        int connect(const std::string &serial_device)
+        {
+            try
+            {
+                // Open the Serial Port at the desired hardware port.
+                serial_port_.Open(serial_device) ;
+            }
+            catch (const LibSerial::OpenFailed&)
+            {
+                std::cerr << "The serial port did not open correctly." << std::endl ;
+                return EXIT_FAILURE ;
+            }
 
-float readEncoder()
-{
-    std::string read_buffer ;
-    sendchr("\r");
-    int encoder_val;
-    try
-    {
-        // Read as many bytes as are available during the timeout period.
-        serial_port.Read(read_buffer, 0, ms_timeout) ;
-    }
-    catch (const ReadTimeout&)
-    {
-        encoder_val = stoi(read_buffer);
-    }
-    return encoder_val*2*PI/ENCODER_TICKS_PER_REV;
-}
+            // Set the baud rate of the serial port.
+            serial_port_.SetBaudRate(LibSerial::BaudRate::BAUD_115200) ;
 
-int setupSerialComm()
-{
-    try
-    {
-        // Open the Serial Port at the desired hardware port.
-        serial_port.Open(SERIAL_PORT_2) ;
-    }
-    catch (const OpenFailed&)
-    {
-        std::cerr << "The serial port did not open correctly." << std::endl ;
-        return EXIT_FAILURE ;
-    }
+            // Set the number of data bits.
+            serial_port_.SetCharacterSize(LibSerial::CharacterSize::CHAR_SIZE_8) ;
 
-    // Set the baud rate of the serial port.
-    serial_port.SetBaudRate(BaudRate::BAUD_115200) ;
+            // Turn off hardware flow control.
+            serial_port_.SetFlowControl(LibSerial::FlowControl::FLOW_CONTROL_NONE) ;
 
-    // Set the number of data bits.
-    serial_port.SetCharacterSize(CharacterSize::CHAR_SIZE_8) ;
+            // Disable parity.
+            serial_port_.SetParity(LibSerial::Parity::PARITY_NONE) ;
+            
+            // Set the number of stop bits.
+            serial_port_.SetStopBits(LibSerial::StopBits::STOP_BITS_1) ;
 
-    // Turn off hardware flow control.
-    serial_port.SetFlowControl(FlowControl::FLOW_CONTROL_NONE) ;
+            return 0;
+        }
+        void disconnect()
+        {
+            serial_port_.Close();
+        }
 
-    // Disable parity.
-    serial_port.SetParity(Parity::PARITY_NONE) ;
-    
-    // Set the number of stop bits.
-    serial_port.SetStopBits(StopBits::STOP_BITS_1) ;
+        bool connected() const
+        {
+            return serial_port_.IsOpen();
+        }
 
-    return 0;
-}
+    private:
+        LibSerial::SerialPort serial_port_ ;
+        size_t ms_timeout = 10 ;
+};
+
 
--- Pico_codes/serial_test/pico_main.cpp
+++ Pico_codes/serial_test/pico_main.cpp
@@ -1,9 +1,15 @@
 #include "pico_comm.hpp"
 
+const std::string SERIAL_PORT_2 = "/dev/ttyACM0" ;
+
 int main()
 {
-    setupSerialComm();
-    writeMotor(1000);
-    sleep(1);
-    std::cout << "\n" << readEncoder()<<std::endl;
+    PicoComms comm_;
+    comm_.connect(SERIAL_PORT_2);
+    comm_.writeMotor(1000);
+    sleep(5);
+    std::cout << "\nConnected: " << comm_.connected()<<std::endl;
+    std::cout << "\n" << comm_.readEncoder()<<std::endl;
+    comm_.disconnect();
+    std::cout << "\nConnected: " << comm_.connected()<<std::endl;
 }
